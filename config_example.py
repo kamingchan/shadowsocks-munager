5c90fdd1de899b59b35c0a065a7971c325965c0d
bazingaterry <terry@32ph.com>
2017-01-21 21:30:27 +0800

    update config example

--- config_example.py
+++ config_example.py
@@ -2,12 +2,13 @@ import logging
 
 URL = 'https://www.example.com/mu'
 KEY = 'your_key'
-UPDATE_TIME = 10
+UPDATE_TIME = 120
 ID = 1
 
 MANAGER_IP = '127.0.0.1'
 MANAGER_PORT = 8888
 
 SOCKET_TIMEOUT = 10
+HTTP_TIMEOUT = 10
 
 logging.basicConfig(level=logging.INFO)
